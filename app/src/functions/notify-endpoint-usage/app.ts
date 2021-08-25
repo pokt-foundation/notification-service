b04e5765626358c1de0b8bc63918f06046fe7b40
Roniel Valdez <roniel_valdez@outlook.com>
2021-08-25 15:52:33 -0400

    [In progress] connect lambda to redis

--- app/src/functions/notify-endpoint-usage/app.ts
+++ app/src/functions/notify-endpoint-usage/app.ts
@@ -1,3 +1,12 @@
+import Redis from 'ioredis'
+
+const redisHost = process.env.REDIS_HOST || ''
+const redisPort = process.env.REDIS_PORT || ''
+
 exports.handler = async () => {
-  console.log('hello world')
+  const redis = new Redis(parseInt(redisPort), redisHost)
+
+  await redis.set('hello', 'world', 'EX', 600)
+
+  console.log(redis.get('hello'))
 };
