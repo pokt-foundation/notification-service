644c10cce5dc4f37183ff2fcd1504a54076727a2
Enrique Ortiz <hi@enriqueortiz.dev>
2021-08-26 13:34:44 -0400

    Usage calculation: Add Influx, date utils (#2)
    * deps: add influx client
    
    * feat: add influx config and query
    
    * feat: write influx script, add date utils
    
    * chore: remove unused function
    
    * feat: add env vars to the template, add testing things
    
    * fix: not loading all files to lambda
    
    * chore: remove log
    
    Co-authored-by: Roniel Valdez <roniel_valdez@outlook.com>

--- app/src/functions/notify-endpoint-usage/app.ts
+++ app/src/functions/notify-endpoint-usage/app.ts
@@ -1,7 +1,18 @@
 import Redis from 'ioredis'
+import { influx, buildAppUsageQuery } from '../../lib/influx'
+import { getUTCTimestamp, getHoursFromNowUtcDate } from '../../lib/date-utils'
 
 const redisHost = process.env.REDIS_HOST || ''
 const redisPort = process.env.REDIS_PORT || ''
+
+export async function getUsageData() {
+  const usage = influx.collectRows(buildAppUsageQuery({
+    start: getHoursFromNowUtcDate(0),
+    stop: getUTCTimestamp()
+  }))
+
+  return usage
+}
 
 exports.handler = async () => {
   const redis = new Redis(parseInt(redisPort), redisHost)
