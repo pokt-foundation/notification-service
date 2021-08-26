1574d0fe9ec0167db5b60063e643a51b346fe6fc
Enrique Ortiz <hi@enriqueortiz.dev>
2021-08-26 12:21:44 -0400

    feat: write influx script, add date utils

--- app/src/functions/notify-endpoint-usage/app.ts
+++ app/src/functions/notify-endpoint-usage/app.ts
@@ -1,7 +1,16 @@
 import Redis from 'ioredis'
+import { influx, buildAppUsageQuery } from '../../lib/influx'
+import { getUTCTimestamp, getHoursFromNowUtcDate } from '../../lib/date-utils'
 
 const redisHost = process.env.REDIS_HOST || ''
 const redisPort = process.env.REDIS_PORT || ''
+
+export async function getUsageData() {
+  return influx.collectRows(buildAppUsageQuery({
+    start: getHoursFromNowUtcDate(0),
+    stop: getUTCTimestamp()
+  }))
+}
 
 exports.handler = async () => {
   const redis = new Redis(parseInt(redisPort), redisHost)
