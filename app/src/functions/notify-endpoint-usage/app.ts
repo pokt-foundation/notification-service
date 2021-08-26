e6ddb970f76978aa8b68e0e2104ac55457d7b109
Roniel Valdez <roniel_valdez@outlook.com>
2021-08-26 10:52:48 -0400

    feat: add vpc config infra to template

--- app/src/functions/notify-endpoint-usage/app.ts
+++ app/src/functions/notify-endpoint-usage/app.ts
@@ -5,8 +5,4 @@ const redisPort = process.env.REDIS_PORT || ''
 
 exports.handler = async () => {
   const redis = new Redis(parseInt(redisPort), redisHost)
-
-  await redis.set('hello', 'world', 'EX', 600)
-
-  console.log(redis.get('hello'))
 };
