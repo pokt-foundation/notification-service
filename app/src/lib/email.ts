140a85164de1b8527c2ab43cadfa25f0bf85c1ae
Enrique Ortiz <hi@enriqueortiz.dev>
2021-08-26 15:23:52 -0400

    feat: add mailgun integration

---
+++ app/src/lib/email.ts
@@ -0,0 +1,58 @@
+import mailgun, { Mailgun, messages } from 'mailgun-js'
+import path from 'path'
+
+const FROM_EMAIL = 'Pocket Portal <portal@pokt.network>'
+const DOMAIN = 'pokt.network'
+
+const WHITELISTED_TEMPLATES = new Map([
+  [
+    'NotificationThresholdHit',
+    [
+      'pocket-dashboard-notifications-threshold-hit',
+      'Pocket Portal: App notification',
+    ],
+  ],
+])
+
+export interface INotificationTemplate {
+  app_name: string
+  app_id: string
+  usage: string
+}
+
+export default class MailgunService {
+  private mailService: Mailgun
+
+  constructor() {
+    this.mailService = mailgun({
+      apiKey: process.env.EMAIL_API_KEY,
+      domain: DOMAIN,
+    })
+  }
+
+  send({
+    templateData,
+    templateName = '',
+    toEmail = '',
+  }: {
+    templateData?: INotificationTemplate
+    templateName: string
+    toEmail: string
+  }): Promise<messages.SendResponse> {
+    const [template, subject] = WHITELISTED_TEMPLATES.get(templateName)
+    const message = {
+      from: FROM_EMAIL,
+      to: toEmail,
+      subject,
+      template,
+      inline: path.join(__dirname, 'portal_logo.png'),
+    }
+
+    if (templateData) {
+      // @ts-ignore
+      message['h:X-Mailgun-Variables'] = JSON.stringify(templateData)
+    }
+
+    return this.mailService.messages().send(message)
+  }
+}
