77bf5b12fd67cbd0e5b3bf7371362d86e0ccc5c5
Roniel Valdez <roniel_valdez@outlook.com>
2021-08-30 15:39:40 -0400

    feat: add ApplicationData type to contain needed info

--- app/src/models/types.ts
+++ app/src/models/types.ts
@@ -1,6 +1,20 @@
+import { StakingStatus } from "@pokt-network/pocket-js"
+
 export type GetUsageDataQuery = {
   relays: number,
   applicationPublicKey: string,
   result: string,
   table: number
+}
+
+export type ApplicationData = {
+  publicKey: string
+  address: string
+  chains: string[],
+  stakedTokens: number,
+  jailed: boolean,
+  status: StakingStatus,
+  maxRelays: number
+  relaysUsed: number,
+  percertageUsed: number
 }
