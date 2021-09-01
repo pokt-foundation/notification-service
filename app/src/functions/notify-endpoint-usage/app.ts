7511bed5515d39da9e7a6bab3e91b67b7178df69
Roniel Valdez <roniel_valdez@outlook.com>
2021-08-31 21:33:26 -0400

    Redis cache (#12)
    * add cache for mongodb calls
    
    * add network call cache
    
    * feat: move fields as env variables

--- app/src/functions/notify-endpoint-usage/app.ts
+++ app/src/functions/notify-endpoint-usage/app.ts
@@ -1,25 +1,30 @@
 import { influx, buildAppUsageQuery } from "../../lib/influx";
 import { getUTCTimestamp, getHoursFromNowUtcDate } from "../../lib/date-utils";
 import connect from "../../lib/db"
-import { ApplicationData, ExtendedApplicationData, ExtendedLoadBalancerData, ExtendedLoadBalancer, GetUsageDataQuery } from "../../models/types";
+import { ApplicationData, ExtendedApplicationData, ExtendedLoadBalancer, ExtendedLoadBalancerData, GetUsageDataQuery } from "../../models/types";
 import User from '../../models/User'
-import { getApplicationNetworkData, getAppsInNetwork } from "../../lib/pocket";
-import { QueryAppResponse, Application } from '@pokt-network/pocket-js';
+import { getAppsInNetwork } from "../../lib/pocket";
+import { Application } from '@pokt-network/pocket-js';
 import ApplicationModel, { IApplication } from "../../models/Application";
 import LoadBalancerModel, { ILoadBalancer } from "../../models/LoadBalancer";
+import Redis from 'ioredis'
 import { retryEvery } from "../../utils/retry";
 
 const redisHost = process.env.REDIS_HOST || "";
 const redisPort = process.env.REDIS_PORT || "";
 
-const maxRetries = process.env.MAX_RETRIES || 3;
+const cacheTTL = parseInt(process.env.NETWORK_CACHE_TTL ?? '') || 3600;
+
+const queryStartTime = parseInt(process.env.INFLUX_QUERY_START_TIME ?? '') || 1;
+
+const redis = new Redis(parseInt(redisPort), redisHost)
 
 const calculateRelaysPercentage = (relays: number, maxRelays: number) => parseFloat(((relays / maxRelays) * 100).toFixed(2))
 
 export async function getUsageData(): Promise<GetUsageDataQuery[]> {
   const usage = (await influx.collectRows(
     buildAppUsageQuery({
-      start: getHoursFromNowUtcDate(1),
+      start: getHoursFromNowUtcDate(queryStartTime),
       stop: getUTCTimestamp(),
     })
   )) as unknown as any[];
@@ -131,7 +136,7 @@ async function getLoadBalancerThreshold(appData: ApplicationData[], dbApps: IApp
   }
 
   appData.forEach(async app => {
-    const dbApp = dbApps.find((data => data.freeTierApplicationAccount.address === app.address))
+    const dbApp = dbApps.find((data => data.freeTierApplicationAccount?.address === app.address))
 
     if (dbApp === undefined) {
       return
@@ -179,18 +184,46 @@ async function getLoadBalancerThreshold(appData: ApplicationData[], dbApps: IApp
 exports.handler = async () => {
   await connect()
 
-  const usage = await getUsageData();
+  const usage = await retryEvery(getUsageData);
+
+  let networkData: Application[]
+
+  const cachedNetworkData = await redis.get('nt-network-apps')
+  if (!cachedNetworkData) {
+    networkData = await retryEvery(getAppsInNetwork)
+    await redis.set('nt-network-apps', JSON.stringify(networkData, (_, value) =>
+      typeof value === 'bigint'
+        ? value.toString()
+        : value
+    ), 'EX', cacheTTL)
+  } else {
+    networkData = JSON.parse(cachedNetworkData)
+  }
 
   const networkApps: Map<string, Application> = new Map<string, Application>()
-    ; (await retryEvery(getAppsInNetwork) as Application[]).forEach(app => networkApps.set(app.publicKey, app))
+  networkData.forEach(app => networkApps.set(app.publicKey, app))
+
+  let dbApps: IApplication[]
+
+  const cachedApps = await redis.get('nt-applications')
+  if (!cachedApps) {
+    dbApps = await ApplicationModel.find()
+    await redis.set('nt-applications', JSON.stringify(dbApps), 'EX', cacheTTL)
+  } else {
+    dbApps = JSON.parse(cachedApps)
+  }
+
+  let loadBalancers: ILoadBalancer[]
+
+  const cachedLoadBalancers = await redis.get('nt-loadBalancers')
+  if (!cachedLoadBalancers) {
+    loadBalancers = await LoadBalancerModel.find()
+    await redis.set('nt-loadBalancers', JSON.stringify(loadBalancers), 'EX', cacheTTL)
+  } else {
+    loadBalancers = JSON.parse(cachedLoadBalancers)
+  }
 
   const appData = getRelaysUsed(networkApps, usage)
-
-  // // TODO: Cache DB application data 
-  const dbApps = await ApplicationModel.find()
-
-  // // TODO: Cache Load Balancer data
-  const loadBalancers = await LoadBalancerModel.find()
 
   const lbData = await getLoadBalancerThreshold(appData, dbApps, loadBalancers, networkApps)
 
